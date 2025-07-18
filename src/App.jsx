e59e0a01691dbcc8c8c679fa1f9f1b10f4ef682c
Khemraj Saud <khemrajsaud56@gmail.com>
2025-07-18 18:00:31 +0545

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./component/home/Home";
-import Navbar from "./component/navbar/navbar";
 import About from "./component/pageses/About";
 import Careers from "./component/pageses/Careers";
 import Footer from "./component/footer/Footer";
@@ -11,12 +10,13 @@ import Contact from "./component/pageses/Contact";
 import Services from "./component/pageses/Services";
 import Maps from "./component/address/Maps";
 import TeamsPage from "./component/pageses/Teams";
+import Navbar from "./component/navbar/navbar";
 
 const App = () => {
   return (
     <div>
       {/* Pass scroll handler to navbar */}
-      <Navbar />
+   <Navbar/>
 
       <Routes>
         <Route path="/service" element={<Services />} />
